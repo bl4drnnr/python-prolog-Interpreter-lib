e1b678eb04c18c1c15093326c23965b964e11382
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-20 14:46:13 +0100

    updated argument parser

--- src/ppil/elem_regex.py
+++ src/ppil/elem_regex.py
@@ -1,4 +1,4 @@
 ELEM_REGEX = r"[A-Za-z0-9_]+|:\-|[()\.,]"
 ATOM_NAME_REGEX = r"^[A-Za-z0-9_]+$"
 VARIABLE_REGEX = r"^[A-Z_][A-Za-z0-9_]*$"
-ARGUMENTS_REGEX = r"\(.*?\)"
+ARGUMENTS_REGEX = r"(?=(\(((?:(?-2)|[^()]+)+)\)))\("
