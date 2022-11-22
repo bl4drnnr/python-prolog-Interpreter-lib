1deda29114b0610349d1e30b0e659fef110573b1
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-11-22 20:30:58 +0100

    basic endpoints

--- src/ppil/api_instance/api.py
+++ src/ppil/api_instance/api.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 
 class ApiInstance:
     def __init__(self, **configs):
         self._app = None
+        self._request_data = None
         self._configs(**configs)
 
     def _configs(self, **configs):
@@ -13,12 +14,24 @@ class ApiInstance:
     def _add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods=None, *args, **kwargs):
         self._app.add_url_rule(endpoint, endpoint_name, handler, methods=methods, *args, **kwargs)
 
-    def _action(self):
-        return "Hello World"
+    def _prolog_to_json(self):
+        self._request_data = request
+
+    def _json_to_prolog(self):
+        self._request_data = request.get_json()
+
+    def _json_execute(self):
+        self._request_data = request.get_json()
+
+    def _prolog_execute(self):
+        self._request_data = request
 
     def run(self, **kwargs):
         self._app = Flask(__name__)
 
-        self._add_endpoint('/action', 'action', self._action, methods=['GET'])
+        self._add_endpoint('/prolog-to-json', 'prolog-to-json', self._prolog_to_json, methods=['POST'])
+        self._add_endpoint('/json-to-prolog', 'json-to-prolog', self._json_to_prolog, methods=['POST'])
+        self._add_endpoint('/json-execute', 'json-execute', self._json_execute, methods=['POST'])
+        self._add_endpoint('/prolog-execute', 'prolog-execute', self._prolog_execute, methods=['POST'])
 
         self._app.run(**kwargs)
