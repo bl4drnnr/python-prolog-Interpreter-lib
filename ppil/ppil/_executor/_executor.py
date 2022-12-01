91f6e71223fbbe238bba8316bf8ab97f5dfbf82d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-01 19:44:16 +0100

    working executor

--- ppil/ppil/_executor/_executor.py
+++ ppil/ppil/_executor/_executor.py
@@ -6,16 +6,30 @@ from os.path import abspath
 from ppil.ppil._api_response_handler import ExecutionError
 
 
+def _wrap_facts(prolog_program):
+    prolog_program = prolog_program.split('.')
+    updated_prolog_program = []
+
+    for item in prolog_program:
+        if ':-' in item:
+            condition_part = item.split(':-')[1]
+            updated_prolog_program.append(
+                item.replace(
+                    condition_part,
+                    f'forall(({condition_part}), format("~q ~a ~n", [X, Y])), halt'
+                )
+            )
+        else:
+            updated_prolog_program.append(item)
+
+    return [item for item in updated_prolog_program if len(item) > 0]
+
+
 class Executor:
     def __init__(self, json_parser, json_format_checker):
         self._json_parser = json_parser
         self._json_format_checker = json_format_checker
         self._current_directory = None
-
-    def _set_current_directory(self):
-        current_directory = abspath(getsourcefile(lambda: 0))
-        current_directory = current_directory.split('/')
-        self._current_directory = '/'.join(current_directory[:-1])
 
     def execute_code(self, code):
         if not code.get('query'):
@@ -32,21 +46,33 @@ class Executor:
         os.chmod(prolog_source_path, 0o700)
         os.chmod(executor_path, 0o700)
 
+        # Here is what I am going to do
+        # If it's pure prolog, split it by dot
+        # Iterate it and find fact, wrap it
+
+        # For JSON first convert to prolog and do steps above
+
+        # Also allow user to send list of queries
+        # What to do with variables
         if isinstance(source_code, str):
-            serialized_program = source_code.replace('\n', '').strip()
+            serialized_program = _wrap_facts(source_code)
         else:
             json_data = self._json_format_checker.check_json_format(code)
             serialized_program = self._json_parser.parse_json(json_data)
 
-        serialized_program = '.\n'.join(serialized_program.split('.'))
+        serialized_program = '.\n'.join(serialized_program) + '.\n'
         source_script_file.write(serialized_program)
         source_script_file.close()
 
         code_query = code.get('query')
-        execution_result = subprocess.run(
-            f"{executor_path} %s %s" % (f"'{code_query}'", prolog_source_path),
-            shell=True,
-            stdout=subprocess.PIPE
-        )
+        execution_result = subprocess.run([
+            'swipl', '-q', '-g', code_query, '-t', 'halt', prolog_source_path
+        ])
 
         return execution_result.stdout.decode('utf-8')
+
+    def _set_current_directory(self):
+        current_directory = abspath(getsourcefile(lambda: 0))
+        current_directory = current_directory.split('/')
+        self._current_directory = '/'.join(current_directory[:-1])
+
